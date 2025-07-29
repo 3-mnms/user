1656ced5a07ae74f9375bed9e4d902c690f4f5c7
정재운 <jaeun101501@gmail.com>
2025-07-29 13:14:06 +0900

    start project

---
+++ src/main/java/com/tekcit/festival/FestivalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tekcit.festival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestivalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestivalServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tekcit/festival/FestivalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tekcit.festival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestivalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
