d36d57bcaa59434df1cbd89a2b48483c13da7e9f
정재운 <jaeun101501@gmail.com>
2025-08-20 15:03:58 +0900

    MNMS-367 feat: 회원 주소 정보 기본 주소지 변경 기능 추가

--- src/main/java/com/tekcit/festival/domain/user/repository/AddressRepository.java
+++ src/main/java/com/tekcit/festival/domain/user/repository/AddressRepository.java
@@ -4,13 +4,16 @@ import com.tekcit.festival.domain.user.entity.Address;
 import com.tekcit.festival.domain.user.entity.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
     List<Address> findAllByUserProfile(UserProfile userProfile);
 
     @Query("SELECT a FROM Address a WHERE a.userProfile.user.userId = :userId")
     List<Address> findAllByUserId(Long userId);
+
+    @Query("SELECT a FROM Address a WHERE a.userProfile.user.userId = :userId AND a.isDefault = true")
+    Optional<Address> findDefaultByUserId(Long userId);
 }
