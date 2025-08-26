526a680d69d6ee04691689544c344f2441487cb7
정재운 <jaeun101501@gmail.com>
2025-08-26 11:14:39 +0900

    MNMS-414 fix: 인가 처리 수정

--- src/main/java/com/tekcit/festival/config/security/SecurityConfig.java
+++ src/main/java/com/tekcit/festival/config/security/SecurityConfig.java
@@ -71,7 +71,8 @@ public class SecurityConfig {
                                 "/api/mail/**",
                                 "/api/auth/kakao/**",
                                 "/api/users/booking-profile/**",
-                                "/api/users/statisticsList"
+                                "/api/users/statisticsList",
+                                "/api/users/reservationList"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
