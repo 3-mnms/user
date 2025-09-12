2a578d686223ffd08d0560628c492501e98774de
CECHERI <24cecheri@gmail.com>
2025-09-12 15:13:46 +0900

    MNMS-668 Bug: booking에서 불러오는 외부 api base url 하드코딩 된 부분 수정

--- src/main/java/com/tekcit/festival/config/security/WebClientConfig.java
+++ src/main/java/com/tekcit/festival/config/security/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.tekcit.festival.config.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,18 +8,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    @Value("${base.service.url}")
+    private String baseServiceUrl;
+
     @Bean
     public WebClient bookingWebClient() {
         return WebClient.builder()
-                .baseUrl("http://localhost:10000")
+                .baseUrl(baseServiceUrl)
                 .build();
     }
-
-    // 다른 외부 서비스 호출을 할 수 있음
-    // @Bean
-    // public WebClient anotherWebClient() {
-    //     return WebClient.builder()
-    //            .baseUrl("http://another-service-url")
-    //            .build();
-    // }
 }
